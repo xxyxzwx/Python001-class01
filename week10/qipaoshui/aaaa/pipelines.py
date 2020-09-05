2cd2424b384e1209aeae2b16ad301d41b3ffa122
xxyxzwx <xxyxzwx@outlook.com>
2020-09-05 20:47:16 +0800

    spider

--- week10/qipaoshui/aaaa/pipelines.py
+++ week10/qipaoshui/aaaa/pipelines.py
@@ -10,7 +10,7 @@ import pymysql, json
 
 class AaaaPipeline:
     def process_item(self, item, spider):
-        with open('/root/learnpython/mysql.json','r') as file:
+        with open('/root/learnpython/scrapy.json','r') as file:
             mysql_conf = json.load(file)
         self.conn = pymysql.connect(
             host = mysql_conf['host'],
@@ -20,15 +20,15 @@ class AaaaPipeline:
             database = mysql_conf['db']
         )
         self.cur = self.conn.cursor()
-        values = [item['productType'], item['productName'], item['username'], \
-                 item['comment_content'], item['comment_time'], item['sentiments'], \
-                 item['username'], item['productName']]
+        values = [item['productType'], item['productName'], item['username'], 
+                 item['comment_content'], item['comment_time'], 
+                 item['sentiments'], item['username'], item['productName']]
         try:
-            sql = 'insert into product(productType, productName, username, \
-                  comment_content, comment_time, sentiments) select \
-                  %s,%s,%s,%s,%s,%s from dual where not \
-                  exists(select username from product where username=(%s) \
-                  and productName=(%s));'
+            sql = ('insert into product(productType, productName, username, '
+                  'comment_content, comment_time, sentiments) select '
+                  '%s,%s,%s,%s,%s,%s from dual where not '
+                  'exists(select username from product where username=(%s) '
+                  'and productName=(%s));')
             self.cur.execute(sql,values)
             self.cur.close()
             self.conn.commit()
